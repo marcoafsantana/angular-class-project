d0e436d381dfddebba70fbd3c6d7513406c0b44a
Marco Santana <marco@capyba.com>
2019-04-18 16:23:36 -0300

    event binding

--- src/app/data-binding/data-binding.component.ts
+++ src/app/data-binding/data-binding.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataBindingComponent implements OnInit {
 
-  url :string = 'google.com';
-  imgUrl: string = "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+  // url :string = 'google.com';
+  // imgUrl: string = "https://images.pexels.com/photos/414612/pexels-photo-414612.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500";
+
+  valorAtual :string = '';
+  isMouseOver :boolean = false;
+
+  botaoClicado(){
+    alert('Clicou');
+  }
+
+  onKeyUp(event: KeyboardEvent){
+    console.log(event.key, (<HTMLInputElement>event.target).value);
+    this.valorAtual = (<HTMLInputElement>event.target).value;
+  }
+
+  salvarValor(value: String){
+    console.log('Salva:', value);
+  }
+
+  onMouseOver(){
+    this.isMouseOver = true;
+  }
   
+  onMouseOut(){
+    this.isMouseOver = false;
+  }
+
   constructor() { }
 
   ngOnInit() {
