0f674711ed11fa42097a0c2dfc658fcd99b603b3
Marco Santana <m.aurelio_santana@hotmail.com>
2019-04-22 17:31:14 -0300

    injecting a service into another service

--- servicesapp/src/app/app.module.ts
+++ servicesapp/src/app/app.module.ts
@@ -4,10 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { CursosComponent } from './cursos/cursos.component';
-import { CursosServices } from './cursos/cursos.service';
-import { CriarCursoComponent } from './criar-curso/criar-curso.component';
-import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
+// import { CursosComponent } from './cursos/cursos.component';
+// import { CursosServices } from './cursos/cursos.service';
+// import { CriarCursoComponent } from './criar-curso/criar-curso.component';
+// import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curso-criado.component';
+import { LogService } from './shared/log.service';
 
 @NgModule({
   declarations: [
@@ -19,6 +20,7 @@ import { ReceberCursoCriadoComponent } from './receber-curso-criado/receber-curs
     CursosModule
   ],
   providers: [
+    LogService
     // CursosServices,
   ],
   bootstrap: [AppComponent]
--- servicesapp/src/app/cursos/cursos.service.ts
+++ servicesapp/src/app/cursos/cursos.service.ts
@@ -1,3 +1,4 @@
+import { LogService } from './../shared/log.service';
 import { Injectable, EventEmitter } from '@angular/core';
 
 @Injectable()
@@ -11,15 +12,17 @@ export class CursosServices {
 
     emitirCursoCriado = new EventEmitter<string>(); //Emite para uma instancia. Caso existam duas instancias desse service, uma não manda para a outra.
     static emitirCursoCriadoSemInstancia = new EventEmitter<string>(); //Com static, esse evento não precisa de instancia pra emitir, assim envia para todas as instancias.
-    constructor(){
+    constructor(private logService : LogService){
         console.log('Service');
     }
 
     getCursos(){
+        this.logService.consoleLog('Obtendo lista de cursos');
         return this.cursos;
     }
 
     addCursos(curso : string){
+        this.logService.consoleLog(`Criando o curso ${curso}`);
         this.cursos.push(curso);
         this.emitirCursoCriado.emit(curso);
         CursosServices.emitirCursoCriadoSemInstancia.emit(curso);
